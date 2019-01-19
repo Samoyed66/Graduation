3b9eb19c494345d6079fff2d3079767743125b11
test <2837390492@qq.com>
2019-01-19 08:07:38 +0800

    修改路径试验推送

--- Barley/src/router/index.js
+++ Barley/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Test from '@/components/lzd/test'
 
 Vue.use(Router)
 var router = new Router({
@@ -9,6 +10,17 @@ var router = new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld,
+      meta: {
+        // 设置头部是否显示
+        headShow: true,
+        // 设置底部是否显示
+        footShow: true
+      }
+    },
+    {
+      path: '/test',
+      name: 'Test',
+      component: Test,
       meta: {
         // 设置头部是否显示
         headShow: true,
